66d9222161bfe6fa86c8f5219a222046c61540a6
Chirag-04 <binarycoderz04@gmail.com>
2024-12-05 12:23:10 +0530

    new changes

---
+++ template.js
@@ -0,0 +1,10 @@
+// Express js server template
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`Server is runing at ${port}`)
+})
+
+//____________________________________________________________
